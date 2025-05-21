7867fce45465a9239f52aacffa6de0ff83925f03
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-05-21 11:52:47 +0100

    Create a header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return(
+        <header>
+            <img src="./public/globe.png" alt="Image of the globe" />
+            <span>my travel journal.</span>
+        </header>
+    );
+}
+
+export default Header;
